58e6591d6fbf9edd1aafafeb84a94bd4ec34a22a
East <yd2004@gmail.com>
2012-03-14 06:29:26 +0000

    默认主题修正

--- themes/default/page.tpl.php
+++ themes/default/page.tpl.php
@@ -11,8 +11,8 @@
 	<div id="wrapper" class="container"><div class="<?php echo $args_id;?>">
 		<div id="header">
       <div class="menu"><?php echo $menu; ?></div>
-      <a href="<?php echo $base_path?>" title="回到 <?php echo $site_global->name; ?> 首页" class="logo" alt="<?php echo $site_global->name; ?>">
-        <img src="<?php echo $site_global->logo; ?>" />
+      <a href="<?php echo $base_path?>" title="回到 <?php echo $site_global->name; ?> 首页" class="logo">
+        <img src="<?php echo $site_global->logo; ?>" alt="<?php echo $site_global->name?>" />
       </a>
     </div>
     
