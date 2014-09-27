5fa6c7a3c93863b8d29b0470c0ba290d6310f8a1
East <yd2004@gmail.com>
2014-09-27 09:32:06 +0000

    修正分页浮动

--- misc/themes/bootstrap/info.php
+++ misc/themes/bootstrap/info.php
@@ -5,6 +5,7 @@ $info = array(
   'name' => 'Twitter bootstrap',
   'styles' => array( // 加载的 css
     'css/bootstrap.min.css',
+    'css/style.css',
   ),
   'scripts' => array( // 加载的 js
     'js/bootstrap.min.js',
